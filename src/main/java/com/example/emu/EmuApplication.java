8cc53240119e9b985821561dddb84fcc4fc0993f
T_T <ice257@yandex.ru>
2023-08-22 09:40:02 +0300

    Initial commit

---
+++ src/main/java/com/example/emu/EmuApplication.java
@@ -0,0 +1,13 @@
+package com.example.emu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmuApplication.class, args);
+    }
+
+}
