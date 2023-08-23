04df2c6aa253822f2607a4bfd4b436fa59603590
T_T <ice257@yandex.ru>
2023-08-23 18:00:41 +0300

    1

--- src/test/java/com/example/emu/Application.java
+++ src/test/java/com/example/emu/Application.java
@@ -2,11 +2,12 @@ package com.example.emu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ import org.springframework.boot.SpringApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }
--- src/test/java/com/example/emu/Controller/UserController.java
+++ src/test/java/com/example/emu/Controller/UserController.java
@@ -1,119 +1,34 @@
 package com.example.emu.Controller;
-
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-//import java.util.Date;
-import java.time.LocalDate;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
-    public class ApiController {
-
-    @GetMapping("/User")
-    public USER getUser() {
-        USER User = new USER("admin", "password");
-        return User;
+public class UserController {
+    @GetMapping("/user")
+    public static User getUser() {
+        return new User("Tanya", "373345");
     }
 
-    @PostMapping("/User")
-    public USER postUser(@RequestBody USER User) {
-        User.getClass(new Date());
-        return User;
-//
-//
-//    }
-//}
+    @PostMapping("/user")
+    public ResponseEntity<Object> postUser(@RequestBody User user) {
+        user.setDate();
+        try {
+            if (user.getLogin() == null || user.getLogin().isEmpty() || user.getPassword() == null || user.getPassword().isEmpty()) {
+                throw new IllegalArgumentException("Неверный формат данных");
+            }
 
-//        @GetMapping("/json")
-//        public String getJson() {
-//            return "{\"Tanya\": \"1789j\"}";
-//
-//
-//        }
-//
-//        @PostMapping("/json")
-//        public ApiResponse postJson(@RequestBody UserCredentials credentials) {
-//            return new ApiResponse(credentials.getLogin(), credentials.getPassword(), LocalDateTime.now());
-//        }
-
-//
-//        public String getData() {
-//            return "{\"key\": \"value\"}";
-//        }
-
-//    public USER get() {
-//        return new USER(this.login, this.password);
-//    }
-//
-//    public USER post(String login, String password) {
-//        USER user = new User(login, password);
-//        user.currentDate = new Date();
-//        return user;
-//    }
-
-
-//        public static class UserCredentials {
-//            private String login;
-//            private String password;
-
-            // getters and setters
-
-//            public String getLogin() {
-//                return login;
-//            }
-//
-//            public void setLogin(String login) {
-//                this.login = login;
-//            }
-//
-//            public String getPassword() {
-//                return password;
-//            }
-//
-//            public void setPassword(String password) {
-//                this.password = password;
-//            }
-//        }
-
-//        public static class ApiResponse {
-//            private String login;
-//            private String password;
-//             private LocalDateTime date;
-
-            // constructors, getters and setters
-
-           // public ApiResponse(String login, String password, LocalDateTime date) {
-             //   this.login = login;
-             //   this.password = password;
-             //   this.date = date;
-          //  }
-
-//            public String getLogin() {
-//                return login;
-//            }
-//
-//            public void setLogin(String login) {
-//                this.login = login;
-//            }
-//
-//            public String getPassword() {
-//                return password;
-//            }
-//
-//            public void setPassword(String password) {
-//                this.password = password;
-//            }
-//
-//            public LocalDateTime getDate() {
-//                return date;
-//            }
-
-           // public void setDate(LocalDateTime date) {
-            //    this.date = date;
-           // }
+            return ResponseEntity.ok(user);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+
+    }
+}
 
 
 
