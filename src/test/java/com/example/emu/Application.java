4be4ad36287b9886470b82052987c90c8e84e6de
T_T <ice257@yandex.ru>
2023-08-24 09:22:30 +0300

    1h

--- src/test/java/com/example/emu/Application.java
+++ src/test/java/com/example/emu/Application.java
@@ -2,7 +2,7 @@ package com.example.emu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- import org.springframework.boot.SpringApplication;
+
 @SpringBootApplication
 public class Application {
 
--- src/test/java/com/example/emu/Controller/UserController.java
+++ src/test/java/com/example/emu/Controller/UserController.java
@@ -1,10 +1,7 @@
 package com.example.emu.Controller;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
